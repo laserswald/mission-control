8403b1a135661704540575117ab729f6e7bf0d15
Ben Davenport-Ray <ben.davenportray@gmail.com>
2025-11-11 12:24:23 -0500

    Fix the server check script and add a de

--- units/system-monitor/system-monitor.sh
+++ units/system-monitor/system-monitor.sh
@@ -4,32 +4,39 @@
 
 set -euo pipefail
 
+DEBUG_MODE=1
+
 SERVERS=(
     "lazr.space 443"
     "mc.lazr.space 25565"
     "mc.lazr.space 25566"
 )
 
-declare -a FAILING_SERVERS
-export FAILING_SERVERS
+FAILING_SERVERS=""
 
 for host in "${SERVERS[@]}"; do
-	if ! nc -w 10 -z $host; then
-	    echo "Could not connect to $host"
-	    FAILING_SERVERS+=("$host\n")
-	fi
+    if ! nc -w 10 -z $host; then
+	echo "Could not connect to $host"
+	FAILING_SERVERS="$FAILING_SERVERS$host\n"
+    fi
 done
 
+# echo $FAILING_SERVERS
 
 # if [[ ${#SERVERS[@]} -ne 0 ]]; then
-if [[ ${#FAILING_SERVERS[@]} -ne 0 ]]; then
-	sendmail -t -F "Lazr.Space System Monitor Bot" <<EOF
+if [[ ${FAILING_SERVERS} || ($DEBUG_MODE -ne 0)]]; then
+    if [ $DEBUG_MODE -ne 0 ]; then	
+	alertcmd="cat"
+    else
+	alertcmd="sendmail -t -F lazr.space"
+    fi
+    $alertcmd <<EOF
 To: me@lazr.space, ben.davenportray@gmail.com
 Subject: [ALERT] Servers unreachable!
 
 The following hosts and ports are currently unreachable:
 
-${FAILING_SERVERS[@]}
+${FAILING_SERVERS}
 
 This alert will continue every so often until it is resolved.
 
